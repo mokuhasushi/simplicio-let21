c103431b595cd0bf058521f9da7894c35dcd63de
Antonio Tirone <tironeantonio@gmail.com>
2021-06-09 18:28:01 +0200

    fix per make

--- test/context.py
+++
@@ -1,9 +0,0 @@
-# Sto provando a impostare la struttura del progetto come suggerito in
-# https://docs.python-guide.org/writing/structure/
-
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', "simplicio")))
-
-import shuntingyard
-import exceptions
