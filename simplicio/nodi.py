2924be0e43e2182a736bdd99f4b92a07e59f83d0
Antonio Tirone <tironeantonio@gmail.com>
2021-06-11 13:48:46 +0200

    Inizio lavoro su semplificatore

--- simplicio/nodi.py
+++ simplicio/nodi.py
@@ -1,104 +1,148 @@
 class Nodo():
-    def __init__(self, value, children=None):
+    def __init__(self, value=None, children=None):
+        self.id = -1
         self.value = value
         if children == None:
             children = []
         self.children = children
-    def visita_df(self, visit, *args):
-        f_r = visit(self, args)
-        for t in children:
-            t.visita_df(visit, args, f_r)
+        self.colore = ""
+        self.boxed = False
     def add_children(self, nodo):
         self.children.append(nodo)
     def get_type(self):
         return "Nodo"
     def get_annotated(self):
-        return {'type':self.get_type()}
+        return {'type':self.get_type(), 'id':self.id, 'colore':self.colore}
     def get_latex(self):
+        main_text = self.get_latex_main()
+        if self.colore != "":
+            return "\\color{" + self.colore + "} {" + main_text + "}"
+        return main_text
+    def get_latex_main(self):
         return ""
+    def numera(self, n):
+        size = len(self.children)
+        if size == 0:
+            self.id = n
+            return n+1
+        elif size == 1:
+            n = self.children[0].numera(n)
+            self.id = n
+            n += 1
+        elif size == 2:
+            n = self.children[0].numera(n)
+            self.id = n
+            n += 1
+            n = self.children[1].numera(n)
+        # Questo caso è pericolo, rovina la ricerca binaria.
+        # Servirà solo se si vorrà unire vari nodi es 3 + 3 + 3 + 1 ...
+        else:
+            for c in self.children:
+                n = c.numera(n)
+            self.id = n
+            n += 1
+        return n
+    # Non so se servono
+    # def colora(self, colore, id=-1):
+    #     def colora_helper(nodo, colore):
+    #         nodo.colore = colore
+    #     self.cerca_id_df(id, colora_helper, colore)
+    #     return self
+    # def decolora(self):
+    #     self.colore = ""
+    # def cerca_id_df(self, id, f, arg=None):
+    #     if id == self.id or id < 0:
+    #         if arg == None:
+    #             f(self)
+    #         else:
+    #             f(self, arg)
+    #     else:
+    #         self.children = [c.cerca_id_df(id, f, arg) for c in self.children]
+    #     return self
     def __repr__(self):
-        return f"{self.get_type()}: value: {self.value}, children:" + \
-            f"{[n for n in self.children]}"
+        return f"{self.get_annotated()}, {[n for n in self.children]}"
 
 
 class NodoParentesiTonde(Nodo):
-    def __init__(self, value, children=None):
+    def __init__(self, value=None, children=None):
         super().__init__(value, children)
     def get_type(self):
         return "ParentesiTonde"
-    def get_latex(self):
-        return f"\\left( {self.children[0].get_latex()} \\right)"
+
+    def get_latex_main(self):
+        main_text = f"\\left( {self.children[0].get_latex()} \\right)"
+        return main_text
 
 class NodoParentesiQuadre(Nodo):
-    def __init__(self, value, children=None):
+    def __init__(self, value=None, children=None):
         super().__init__(value, children)
     def get_type(self):
         return "ParentesiQuadre"
-    def get_latex(self):
+    def get_latex_main(self):
         return f"\\left[ {self.children[0].get_latex()} \\right]"
 
 class NodoParentesiGraffe(Nodo):
-    def __init__(self, value, children=None):
+    def __init__(self, value=None, children=None):
         super().__init__(value, children)
     def get_type(self):
         return "ParentesiGraffe"
-    def get_latex(self):
+    def get_latex_main(self):
         return "\\left\{" + f" {self.children[0].get_latex()} " + "\\right\}"
 
 class NodoAddizione(Nodo):
-    def __init__(self, value, children=None):
+    def __init__(self, value=None, children=None):
         super().__init__(value, children)
     def get_type(self):
         return "Addizione"
-    def get_latex(self):
+    def get_latex_main(self):
         return f"{self.children[0].get_latex()} + {self.children[1].get_latex()}"
 
 class NodoSottrazione(Nodo):
-    def __init__(self, value, children=None):
+    def __init__(self, value=None, children=None):
         super().__init__(value, children)
     def get_type(self):
         return "Sottrazione"
-    def get_latex(self):
+    def get_latex_main(self):
         return f"{self.children[0].get_latex()} - {self.children[1].get_latex()}"
 
 class NodoMoltiplicazione(Nodo):
-    def __init__(self, value, children=None):
+    def __init__(self, value=None, children=None):
         super().__init__(value, children)
     def get_type(self):
         return "Moltiplicazione"
-    def get_latex(self):
+    def get_latex_main(self):
         return f"{self.children[0].get_latex()} \\times {self.children[1].get_latex()}"
 
 class NodoDivisione(Nodo):
-    def __init__(self, value, children=None):
+    def __init__(self, value=None, children=None):
         super().__init__(value, children)
     def get_type(self):
         return "Divisione"
-    def get_latex(self):
+    def get_latex_main(self):
         return f"{self.children[0].get_latex()} : {self.children[1].get_latex()}"
 
 class NodoMenoUnario(Nodo):
-    def __init__(self, value, children=None):
+    def __init__(self, value=None, children=None):
         super().__init__(value, children)
     def get_type(self):
         return "MenoUnario"
-    def get_latex(self):
+    def get_latex_main(self):
         return f"- {self.children[0].get_latex()}"
 
 class NodoFrazione(Nodo):
-    def __init__(self, value, children=None):
+    def __init__(self, value=None, children=None):
         super().__init__(value, children)
     def get_type(self):
         return "Frazione"
-    def get_latex(self):
+    def get_latex_main(self):
         return f"\\frac{{{self.children[0].get_latex()}}} {{{self.children[1].get_latex()}}}"
 
 class NodoPotenza(Nodo):
-    def __init__(self, value, children=None):
+    def __init__(self, value=None, children=None):
         super().__init__(value, children)
     def get_type(self):
         return "Potenza"
-    def get_latex(self):
+    def get_latex_main(self):
         return f"{self.children[0].get_latex()} ^ {{{self.children[1].get_latex()}}}"
 
 class NodoNumero(Nodo):
@@ -107,6 +151,8 @@ class NodoNumero(Nodo):
     def get_type(self):
         return "Numero"
     def get_annotated(self):
-        return {'type': self.get_type(), 'value': self.value}
-    def get_latex(self):
+        d = super().get_annotated()
+        d['value'] = self.value
+        return d
+    def get_latex_main(self):
         return f"{self.value}"
---
+++ simplicio/semplificatore.py
@@ -0,0 +1,81 @@
+import nodi
+
+class Semplificatore():
+    # Non sono sicuro di questo approccio. Il vantaggio è che se dovessi
+    # cambiare la stringa di rappresentazione dei tipi qui non dovrei cambiare
+    # nulla, ma perchè dovrei farlo?
+    tipi_nodi = {
+        "nodo"  : nodi.Nodo().get_type(),
+        "tonde" : nodi.NodoParentesiTonde().get_type(),
+        "quadre": nodi.NodoParentesiQuadre().get_type(),
+        "graffe": nodi.NodoParentesiGraffe().get_type(),
+        "add"   : nodi.NodoAddizione().get_type(),
+        "sub"   : nodi.NodoSottrazione().get_type(),
+        "mul"   : nodi.NodoMoltiplicazione().get_type(),
+        "div"   : nodi.NodoDivisione().get_type(),
+        "meno"  : nodi.NodoMenoUnario().get_type(),
+        "frac"  : nodi.NodoFrazione().get_type(),
+        "pow"   : nodi.NodoPotenza().get_type(),
+        "value" : nodi.NodoNumero(0).get_type()
+    }
+    def __init__(self, root):
+        # dizionario: tipo -> nodi di quel tipo
+        self.nodes_type2node_id = {
+            self.tipi_nodi['nodo'] : [],
+            self.tipi_nodi['tonde'] : [],
+            self.tipi_nodi['quadre'] : [],
+            self.tipi_nodi['graffe'] : [],
+            self.tipi_nodi['add'] : [],
+            self.tipi_nodi['sub'] : [],
+            self.tipi_nodi['mul'] : [],
+            self.tipi_nodi['div'] : [],
+            self.tipi_nodi['meno'] : [],
+            self.tipi_nodi['frac'] : [],
+            self.tipi_nodi['pow'] : [],
+            self.tipi_nodi['value'] : []
+        }
+        self.root = root
+        self.root.numera(0)
+        self.get_nodes_type(root)
+    # Percorre ricorsivamente l'albero e salva i nodi per tipo
+    def get_nodes_type(self, tree):
+        self.nodes_type2node_id[tree.get_type()].append(tree.id)
+        for c in tree.children:
+            self.get_nodes_type(c)
+    # risolve tutta l'equazione, a partire dalle parentesi
+    def solve(self):
+        texts = ["&"+self.root.get_latex()]
+        tree = self.root
+        for tipo_parentesi in [self.nodes_type2node_id[self.tipi_nodi['tonde']],
+                  self.nodes_type2node_id[self.tipi_nodi['quadre']],
+                  self.nodes_type2node_id[self.tipi_nodi['graffe']]]:
+            for nodo_id in tipo_parentesi:
+                par, cur = self.trova_nodo(nodo_id)
+                cur.colore = "blue"
+# #                self.root = self.root.colora("blue", nodo_id)
+#                 if par.children[0].id == cur.id:
+#                     par.children[0] = cur
+#                 else:
+#                     par.children[1] = cur
+                texts.append(self.root.get_latex())
+                cur.colore = ""
+        return texts
+    def trova_nodo(self, id):
+        par, cur = self.root, self.root
+        if id == self.root.id :
+            return par, cur
+        # assumo albero binario TODO
+        if id < par.id:
+            cur = par.children[0]
+        else:
+            cur = par.children[1]
+        while id != cur.id:
+            par = cur
+            if id < par.id:
+                cur = par.children[0]
+            else:
+                cur = par.children[1]
+        return par,cur
+    # risolve una parentesi ?
+    def step(self, id):
+        pass
